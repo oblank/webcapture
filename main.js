38be33e0caea356067905ef5ebf8dbba1c477f9d
oBlank <dyh1919@gmail.cm>
2020-09-11 11:40:44 +0800

    界面

--- main.js
+++ main.js
@@ -1,12 +1,13 @@
 // Modules to control application life and create native browser window
 const {app, BrowserWindow} = require('electron')
 const path = require('path')
+const captureWebsite = require('capture-website');
 
 function createWindow () {
   // Create the browser window.
   const mainWindow = new BrowserWindow({
     width: 700,
-    height: 500,
+    height: 550,
     webPreferences: {
       preload: path.join(__dirname, 'preload.js')
     }
@@ -30,13 +31,15 @@ app.whenReady().then(() => {
     // dock icon is clicked and there are no other windows open.
     if (BrowserWindow.getAllWindows().length === 0) createWindow()
   })
+
+  captureWebsite.file('https://baidu.com', 'baidu.png');
 })
 
 // Quit when all windows are closed, except on macOS. There, it's common
 // for applications and their menu bar to stay active until the user quits
 // explicitly with Cmd + Q.
 app.on('window-all-closed', function () {
-  if (process.platform !== 'darwin') app.quit()
+  // if (process.platform !== 'darwin') app.quit()
 })
 
 // In this file you can include the rest of your app's specific main process
